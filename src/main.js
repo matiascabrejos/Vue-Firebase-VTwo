3be64617e3da4fa7e7769a84e17fdac4c0a99f28
matiascabrejos <matias.cabrejos@hotmail.com>
2021-10-17 21:43:22 -0300

    Adding router to the project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import store from './store/index.js'
 
@@ -7,8 +8,17 @@ import TheButton from './components/TheButton.vue'
 
 const app = createApp(App)
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', redirect: '/home' }
+    ],
+    linkActiveClass: 'active',
+  })
+
 app.component('the-login', TheLogin)
 app.component('the-button', TheButton)
 
+app.use(router)
 app.use(store)
 app.mount('#app')
