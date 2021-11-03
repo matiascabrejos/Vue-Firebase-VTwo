905b01d5462dfbca1969579a807e8cb8e1766b49
matiascabrejos <matias.cabrejos@hotmail.com>
2021-11-02 22:13:08 -0300

    Making some style changes to the navbar and login component

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,33 @@
 <template>
-  <router-link to="/">HOME</router-link>
+  <div class="font-sans antialiased" id="app">
+  <nav class="flex items-center justify-between flex-wrap bg-black p-6">
+    <div class="flex items-center flex-no-shrink text-white mr-6">
+      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    </div>
+    <div class="block sm:hidden">
+      <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+      <div class="text-sm sm:flex-grow">
+        <router-link to="/home" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-blue-500  hover:text-white mr-4">
+          HOME
+        </router-link>
+        <router-link to="/login" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-blue-500 hover:text-white mr-4">
+          LOGIN
+        </router-link>
+      </div>
+        <div v-if="isLoggedIn">
+          <button @click="logout" class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-blue-500 border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0">
+            Logout
+          </button>          
+        </div>
+    </div>
+  </nav>
+</div>
+  
   <router-link to="/login">LOGIN</router-link>
   <div v-if="isLoggedIn">
     <button @click="logout">LOGOUT</button>
@@ -9,6 +37,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      open: false
+    }
+  },
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated
@@ -17,6 +50,9 @@ export default {
   methods: {
     logout() {
       this.$store.dispatch('logout');
+    },
+    toggle() {
+      this.open = !this.open
     }
   }
 }
